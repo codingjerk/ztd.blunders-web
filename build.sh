704427c36aeaad035cfa8b4efa938bc10a54fe55
Denis Gruzdev <evilinfuture@gmail.com>
2015-05-10 10:18:48 +0100

    Switched to uglifyjs for compress and closure compiler to get errors and warnings

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ sed '/\.min\.css/b; /\.min\.js/b; s/\.css/\.min\.css/g; s/\.js/\.min\.js/g' inde
 csso css/main.css css/main.min.css
 csso chessboardjs/css/chessboard.css chessboardjs/css/chessboard.min.css
 
-closure --js js/main.js > js/main.min.js
-closure --js chessboardjs/js/chessboard.js > chessboardjs/js/chessboard.min.js
-# closure make this library unusable, check it. sed '253,/,$/s/,$//;443,/turn$/s/turn$/turn,/;1415,1416d;1417s/.*/}/' chessboardjs/js/chessboard.js | closure -O WHITESPACE_ONLY >
-cp chessboardjs/js/chess.js chessboardjs/js/chess.min.js
+# Compile main.js with closure to get errors and warnings
+closure --js js/main.js > /dev/null
+
+uglifyjs -cm -- js/main.js > js/main.min.js
+uglifyjs -cm -- chessboardjs/js/chessboard.js > chessboardjs/js/chessboard.min.js
+uglifyjs -cm -- chessboardjs/js/chess.js > chessboardjs/js/chess.min.js
