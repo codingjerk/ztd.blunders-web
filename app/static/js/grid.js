613fe9633a6f7f7a62a3ded3bf49f2e2fe01d076
indesege <indesege@gmail.com>
2015-06-06 04:18:47 +0100

    Modules grid and utils added, profile upgraded

---
+++ app/static/js/grid.js
@@ -0,0 +1,95 @@
+var grid = {};
+
+(function (module) {
+    var generateCaption = function(caption) {
+        return '<tr class="caption-row"><td class="caption-block" colspan="3">{0}</td></tr>'
+            .format(caption);
+    }
+
+    var generateNamedRow = function(rowData) {
+        return '<tr class="info-row"><td class="info-block-wrapper" colspan="3"><div id="{0}"></div></td></tr>'
+            .format(rowData.id);
+    }
+
+    var generateCell = function(cell) {
+        return ('<td class="grid-block">' +
+            '<div class="grid-label">{0}</div>' +
+            '<div class="grid-value" id="{1}"></div>' +
+            '<div class="grid-additional">{2}</div>' +
+            '</td>').format(cell.label, cell.id, cell.additional);
+    }
+
+    var generateRow = function(rowData) {
+        if (rowData.type === 'cell') {
+            return generateCell(rowData);
+        } else if (rowData.type === 'chart') {
+            return generateNamedRow(rowData);
+        }
+
+        console.log('Unknown row type');
+    }
+
+    var parseNamedRows = function(rowsData) {
+        var result = '';
+
+        while (rowsData.length > 0) {
+            if (rowsData[0].type !== 'chart') break;
+
+            var rowData = rowsData.shift();
+            result += generateRow(rowData);
+        }
+
+        return result;
+    }
+
+    var parseCells = function(rowsData) {
+        var cells = [];
+
+        while (rowsData.length > 0) {
+            if (rowsData[0].type !== 'cell') break;
+
+            var rowData = rowsData.shift();
+            cells.push(generateRow(rowData));
+        }
+
+        var result = '';
+
+        while (cells.length > 0) {
+            var c1 = cells.shift() || '';
+            var c2 = cells.shift() || '';
+            var c3 = cells.shift() || '';
+
+            result += '<tr class="grid-row">{0}{1}{2}</tr>'.format(c1, c2, c3);
+        }
+
+        return result;
+    }
+
+    var generateRows = function(rowsData) {
+        var result = '';
+
+        while (rowsData.length > 0) {
+            result += parseNamedRows(rowsData);
+            result += parseCells(rowsData);
+        }
+
+        return result;
+    }
+
+    var generateBlock = function(block) {
+        var caption = generateCaption(block.caption);
+        var body = generateRows(block.rows);
+
+        return '<table class="details-block">{0}{1}</table>'.format(caption, body);
+    }
+
+    module.generate = function(blocks) {
+        var result = '';
+
+        blocks.forEach(function(block) {
+            result += generateBlock(block);
+        });
+
+        return result;
+    }
+})(grid);
--- app/static/js/profile.js
+++ app/static/js/profile.js
@@ -1,37 +1,84 @@
 var cosPoints = []; 
-for (var i=0; i<2*Math.PI; i+=0.1){ 
+for (var i=0; i<50*Math.PI; i+=0.1){ 
     cosPoints.push([i, Math.cos(i)]); 
 } 
 
-$('#temp-block').css('width', '100%');
-$('#temp-block').css('height', '200px');
-$('#temp-block2').css('width', '100%');
-$('#temp-block2').css('height', '200px');
-
-var plot2 = $.jqplot('temp-block', [cosPoints], {  
-      series:[{showMarker:false}],
-      axes:{
-        xaxis:{
-          labelRenderer: $.jqplot.CanvasAxisLabelRenderer
-        },
-        yaxis:{
-          labelRenderer: $.jqplot.CanvasAxisLabelRenderer
-        }
-      }
-  });
-
-var plot2 = $.jqplot('temp-block2', [cosPoints], {  
-      series:[{showMarker:false}],
-      axes:{
-        xaxis:{
-          labelRenderer: $.jqplot.CanvasAxisLabelRenderer
-        },
-        yaxis:{
-          labelRenderer: $.jqplot.CanvasAxisLabelRenderer
-        }
-      }
-  });
-
 (function() {
-    
+    function drawChart(id, data) {
+        $.jqplot(id, data, {  
+            series: [{showMarker: false}],
+            axes: {
+                xaxis: {
+                    labelRenderer: $.jqplot.CanvasAxisLabelRenderer
+                },
+                yaxis: {
+                    labelRenderer: $.jqplot.CanvasAxisLabelRenderer
+                }
+            }
+        });
+    }
+
+    blocks = [{
+        caption: 'Blunders',
+        rows: [
+            {
+                type: 'chart', 
+                id: 'rating-chart'
+            },
+            {
+                type: 'cell',
+                label: 'Failed',
+                id: 'failed-blunders-value',
+                additional: 'Last week'
+            },
+            {
+                type: 'cell',
+                label: 'Total',
+                id: 'total-blunders-value',
+                additional: 'Last week'
+            },
+            {
+                type: 'cell',
+                label: 'Solved',
+                id: 'solved-blunders-value',
+                additional: 'Last week'
+            }
+        ]
+    }];
+
+    var html = grid.generate(blocks);
+    $('#details').html(html);
+
+    ajaxDummy = [
+        {
+            id: 'rating-chart',
+            value: [cosPoints]
+        },
+        {
+            id: 'failed-blunders-value',
+            value: 124
+        },
+        {
+            id: 'total-blunders-value',
+            value: 321
+        },
+        {
+            id: 'solved-blunders-value',
+            value: 321-124
+        }
+    ];
+
+    function onRequest(data) {
+        for (var i = 0; i < data.length; i++) {
+            var element = data[i];
+
+            if (element.id === 'rating-chart') {
+                drawChart('rating-chart', element.value);
+            } else {
+                $('#' + element.id).html(element.value);
+            }
+        }
+    }
+
+    onRequest(ajaxDummy);
 })();
