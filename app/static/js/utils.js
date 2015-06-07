6a2227d4dd2294ac0cc069bb467ea85f1da9b7b9
indesege <indesege@gmail.com>
2015-06-07 14:20:05 +0100

    utils.js now is module and have map, mapNear functions and fixDate

--- app/static/js/utils.js
+++ app/static/js/utils.js
@@ -1,4 +1,6 @@
-(function() {
+var utils = {};
+
+(function(module) {
     String.prototype.format = function() {
         var str = this;
         for (var i = 0; i < arguments.length; i++) {
@@ -7,4 +9,26 @@
         }
         return str;
     }
-})();
+
+    Array.prototype.map = function(f) {
+        result = [];
+        for (var i = 0; i < this.length; ++i) {
+            result.push(f(this[i]));
+        }
+
+        return result;
+    }
+
+    Array.prototype.mapNear = function(index, f) {
+        return this.map(function(e) {
+            var result = e;
+            result[index] = f(result[index]);
+
+            return e;
+        });
+    }
+
+    module.fixDate = function(rawDate) {
+        return new Date(rawDate);
+    }
+})(utils);
