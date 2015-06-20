fd50f66c6223b02044118ccf753eb37511cc9d7c
indesege <indesege@gmail.com>
2015-06-20 08:07:24 +0100

    Full validation for new users added

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,3 +1,5 @@
+import re
+
 class Roles:
     ADMIN = 0
     USER = 3
@@ -29,4 +31,50 @@ def jsonifyBlunder(data):
         }
 
     }
-    
+
+def validateUsername(username):
+    if len(username) < 3:
+        return "Username must contains at least 3 letter" 
+
+    if not re.match("^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*$", username):
+        return "Your name is too strange"
+
+    return None
+
+def validatePassword(password):
+    if len(password) < 5:
+        return "Your password must be at least 5 characters long"
+
+    return None
+
+def validateEmail(email):
+    if len(email.strip()) == 0: return
+
+    if not re.match('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$', email):
+        return "Invalid email"
+
+    return None
+
+def validateUser(username, password, email):
+    usernameValidation = validateUsername(username)
+    if usernameValidation is not None: return {
+        'status': 'error',
+        'field': 'username', 
+        'message': usernameValidation
+    }
+
+    passwordValidation = validatePassword(password)
+    if passwordValidation is not None: return {
+        'status': 'error',
+        'field': 'password', 
+        'message': passwordValidation
+    }
+
+    emailValidation = validateEmail(email)
+    if emailValidation is not None: return {
+        'status': 'error',
+        'field': 'email', 
+        'message': emailValidation
+    }
+
+    return None
