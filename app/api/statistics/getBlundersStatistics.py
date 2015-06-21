a5ebbe603e21503ed1afd1e47374c16a2824f2ae
JackalSh <jackalsh@gmail.com>
2015-06-21 19:35:09 +0300

    Minor fix.

--- app/api/statistics/getBlundersStatistics.py
+++ app/api/statistics/getBlundersStatistics.py
@@ -8,9 +8,14 @@ from app import utils
 
 @app.route('/statistics/getBlundersStatistics', methods=['POST'])
 def getBlundersStatistics():
+    username = None
     try:
         username = request.json['username']
-        return jsonify(postgre.getBlundersStatistics(username)) # user statistics
     except:
+        pass
+
+    if username is None:
         return jsonify(db.getBlundersStatistics())              # server statistics
+    else:
+        return jsonify(postgre.getBlundersStatistics(username)) # user statistics
     
