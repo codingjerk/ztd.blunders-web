bb079dc3b8bd5d41aa4215a3842bfb980077d3ac
indesege <indesege@gmail.com>
2015-06-23 10:50:44 +0100

    Lost fixes

--- app/views/training.py
+++ app/views/training.py
@@ -2,6 +2,8 @@ from flask import render_template
 from app import app
 from app.utils import session
 
+@app.route('/')
+@app.route('/index')
 @app.route('/training')
 def training():
     return render_template('training.html', title = 'Ztd.Blunders', session = session)
