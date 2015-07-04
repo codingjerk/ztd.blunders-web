9987d5256d5a70d1867772ca0b5af028595d2273
Denis Gruzdev <chezstov@gmail.com>
2015-07-05 00:20:00 +0300

    Explore dummy now ready

--- app/views/explore.py
+++ app/views/explore.py
@@ -2,8 +2,10 @@ from flask import render_template
 from app import app
 from app.utils import session
 
-@app.route('/explore')
-@app.route('/explore/')
 @app.route('/explore/<blunderId>')
 def explore(blunderId): #pylint: disable=unused-argument
-    return render_template('download.html', title = 'Ztd.Blunders', session = session)
+    return render_template('explore.html', title = 'Ztd.Blunders', session = session)
+
+@app.route('/explore')
+def explore_empty():
+    return explore(None)
