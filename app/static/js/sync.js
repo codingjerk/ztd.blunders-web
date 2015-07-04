3e2f65c7cec19576c9f89e794336b41e50a323b7
Denis Gruzdev <chezstov@gmail.com>
2015-07-04 22:31:42 +0300

    Added delay to animate sync.ajax

--- app/static/js/sync.js
+++ app/static/js/sync.js
@@ -1,27 +1,35 @@
 var sync = {};
 
 (function initModule(module) {
-	var syncTemplate = '<i class="fa fa-circle-o-notch fa-spin"></i>';
+    var syncTemplate = '<i class="fa fa-circle-o-notch fa-spin"></i>';
 
-	module.ajax = function(args) {
-		var syncElement = $('#' + args.id);
-		if (syncElement.data('busy')) return;
-		syncElement.data('busy', true);
+    module.ajax = function(args) {
+        var syncElement = $('#' + args.id);
+        if (syncElement.data('busy')) return;
+        syncElement.data('busy', true);
 
-		var savedContent = syncElement.html();
+        var savedContent = syncElement.html();
 
-		syncElement.html(syncTemplate);
+        setTimeout(function() {
+            if (!syncElement.data('busy')) return;
+
+            syncElement.data('animated', true);
+            syncElement.html(syncTemplate);
+        }, 100);
 
         $.ajax({
-        	type: args.type || 'POST',
+            type: args.type || 'POST',
             url: args.url,
             contentType: args.contentType || 'application/json',
             data: JSON.stringify(args.data)
         }).done(function(data) {
-        	syncElement.html(savedContent);  
-			syncElement.data('busy', false);
+            syncElement.data('busy', false);
 
-        	args.onDone(data);
+            if (syncElement.data('animated')) {
+                syncElement.html(savedContent);
+            }
+
+            args.onDone(data);
         });
-	};
+    };
 })(sync);
